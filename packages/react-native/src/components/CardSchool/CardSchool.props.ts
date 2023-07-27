17028523cce654df9ab957c26e19479027a22a02
˜cesarvargasjr˜ <cesar_vargas_jr@hotmail.com>
2023-07-27 18:11:48 -0300

    feat: card school

---
+++ packages/react-native/src/components/CardSchool/CardSchool.props.ts
@@ -0,0 +1,20 @@
+export interface CardProps {
+  title?: string
+  local?: string
+  text?: string
+  tag?: string
+  colorTitle?: string
+  colorLocal?: string
+  colorText?: string
+  sizeTitle?: number
+  sizeText?: number
+  icon?: any
+  borderRadius?: number
+  height?: number
+  width?: string | number
+  marginTop?: number
+  marginBottom?: number
+  colorIcon?: string
+  onPress?: any
+  iconIsPressed?: any
+}
