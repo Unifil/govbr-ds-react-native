1cc8e818f5f14bccb37000bcf4bbd45ed5ccdc91
˜cesarvargasjr˜ <cesar_vargas_jr@hotmail.com>
2023-08-30 01:27:01 -0300

    feat: create-tooltip

---
+++ packages/react-native/src/components/Tooltip/Tooltip.props.ts
@@ -0,0 +1,10 @@
+export interface TooltipProps {
+  title?: string
+  text?: string
+  top?: number
+  right?: number
+  bottom?: number
+  left?: number
+  iconClose?: any
+  positionArrow?: 'topLeft' | 'topRight' | 'bottomLeft' | 'bottomRight'
+}
---
+++ packages/react-native/src/components/Tooltip/Tooltip.styles.ts
@@ -0,0 +1,65 @@
+import { colors, fontSizes, fontWeights } from '@unifil/tokens'
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  containerScreen: {
+    position: 'absolute',
+    height: '100%',
+    width: '100%',
+    backgroundColor: '#00000057',
+    zIndex: 1
+  },
+  containerTooltip: {
+    position: 'absolute',
+    backgroundColor: colors.white,
+    padding: 16,
+    borderRadius: 8,
+    top: 100,
+    right: 100,
+    bottom: 300,
+    left: 100,
+    zIndex: 9999999,
+    elevation: 1,
+    shadowColor: 'rgba(0, 0, 0, 0.12)',
+    shadowOffset: {
+      width: 2,
+      height: 4
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 3
+  },
+  containerTitle: {
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginBottom: 12
+  },
+  title: {
+    fontSize: fontSizes.sm,
+    fontWeight: fontWeights.bold,
+    fontFamily: 'Rawline-Bold',
+    color: colors.blue
+  },
+  text: {
+    fontSize: fontSizes.sm,
+    fontWeight: fontWeights.regular,
+    fontFamily: 'Rawline-Regular',
+    color: colors.blue
+  },
+  arrow: {
+    position: 'absolute',
+    top: -18,
+    left: 20,
+    bottom: 0,
+    right: 0,
+    width: 0,
+    height: 0,
+    borderLeftWidth: 10,
+    borderRightWidth: 10,
+    borderBottomWidth: 18,
+    borderLeftColor: 'transparent',
+    borderRightColor: 'transparent',
+    borderBottomColor: colors.white,
+    transform: [{ rotate: '0deg' }]
+  }
+})
