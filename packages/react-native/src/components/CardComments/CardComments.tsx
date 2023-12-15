b90a5c30688341542a4e6ae0fc1d17005848a6b5
cesarvargasjr <cesar_vargas_jr@hotmail.com>
2023-12-15 19:16:36 -0300

    fix: updates-component-card-comments

--- packages/react-native/src/components/CardComments/CardComments.tsx
+++ packages/react-native/src/components/CardComments/CardComments.tsx
@@ -6,51 +6,98 @@ import { Text, TouchableOpacity, View } from 'react-native'
 import { CardCommentsProps } from './CardComments.props'
 import { styles } from './CardComments.styles'
 
-export const CardComments = (props: CardCommentsProps): ReactElement => {
+export const CardComments = ({
+  comment,
+  date,
+  status,
+  colorStatus,
+  onPressDelete,
+  iconButtonDelete,
+  disabledDelete,
+  onPressUpdate,
+  iconButtonUpdate,
+  disabledUpdate,
+  onPressCheck,
+  iconButtonCheck,
+  disabledCheck
+}: CardCommentsProps): ReactElement => {
   return (
-    <View {...props}>
+    <View>
       <View style={styles.containerCard}>
         <View style={styles.row}>
           <View style={styles.containerDate}>
             <Text style={styles.textDate}>
-              {props?.date}
+              {date}
             </Text>
           </View>
           <View
             style={[
               styles.containerStatus,
               {
-                backgroundColor: props?.colorStatus ? props?.colorStatus : colors.gray300
+                backgroundColor: colorStatus || colors.gray300
               }
             ]}
           >
             <Text style={styles.textStatus}>
-              {props.status}
+              {status}
             </Text>
           </View>
         </View>
         <Text style={styles.textComment}>
-          {props.comment}
+          {comment}
         </Text>
         <View style={styles.containerButtons}>
-          {props?.buttons?.map((item) =>
+          {onPressDelete &&
             <TouchableOpacity
-              key={item?.id}
-              onPress={item?.onPress}
-              disabled={item?.disabled}
+              onPress={onPressDelete}
+              disabled={disabledDelete}
               style={[
                 styles.button,
                 {
-                  backgroundColor: item?.colorButton,
-                  opacity: item?.disabled ? 0.3 : 1
+                  backgroundColor: colors.red,
+                  opacity: disabledDelete ? 0.3 : 1
                 }
               ]}
             >
               <View>
-                {item?.iconButton}
+                {iconButtonDelete}
               </View>
             </TouchableOpacity>
-          )}
+          }
+          {onPressCheck &&
+            <TouchableOpacity
+              onPress={onPressCheck}
+              disabled={disabledCheck}
+              style={[
+                styles.button,
+                {
+                  backgroundColor: colors.green,
+                  opacity: disabledCheck ? 0.3 : 1
+                }
+              ]}
+            >
+              <View>
+                {iconButtonCheck}
+              </View>
+            </TouchableOpacity>
+          }
+          {onPressUpdate &&
+            <TouchableOpacity
+              onPress={onPressUpdate}
+              disabled={disabledUpdate}
+              style={[
+                styles.button,
+                {
+                  backgroundColor: colors.blueSecondary,
+                  opacity: disabledUpdate ? 0.3 : 1
+                }
+              ]}
+            >
+              <View>
+                {iconButtonUpdate}
+              </View>
+            </TouchableOpacity>
+          }
         </View>
       </View>
     </View>
