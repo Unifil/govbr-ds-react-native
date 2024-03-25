abd32342c8120dbd1a2ff00379053e6fb91e1145
ermessonlima <ermessonlimadossantos@hotmail.com>
2024-03-25 03:24:54 -0300

    fix: header adjustment

--- packages/react-native/src/components/Header/Header.props.ts
+++ packages/react-native/src/components/Header/Header.props.ts
@@ -7,5 +7,6 @@ export interface HeaderProps {
   children?: React.ReactNode
   backgroundColor?: string
   iconHelp?: React.ReactNode
+  hideStatusBar?: boolean
   onPressHelp?: () => void
 }
--- packages/react-native/src/components/Header/Header.tsx
+++ packages/react-native/src/components/Header/Header.tsx
@@ -8,7 +8,11 @@ import { normalize } from '../../utils/normalize'
 
 export const Header = (props: HeaderProps): ReactElement => {
   return (
-    <>
+    <View
+      style={{
+        backgroundColor: props?.hideStatusBar ? props?.backgroundColor : 'transparent'
+      }}
+    >
       {props?.backgroundColor && <View style={styles.statusBar} />}
       <View
         style={[
@@ -24,25 +28,25 @@ export const Header = (props: HeaderProps): ReactElement => {
             styles.content,
             {
               backgroundColor: props?.backgroundColor,
-              marginBottom: props?.backgroundColor ? normalize(50) : 0
+              marginBottom: props?.hideStatusBar ? normalize(-60) : props?.backgroundColor ? normalize(50) : 0
             }
           ]}
         >
-        <View style={styles.box}>
-          {props.onPress && (
-            <TouchableOpacity onPress={props.onPress}>
-              {props.icon}
-            </TouchableOpacity>
-          )}
-          <View style={styles.containerText}>
-            <Text style={styles.title}>
-              {props.title}
-            </Text>
-            <Text style={styles.subtitle}>
-              {props.subtitle}
-            </Text>
+          <View style={styles.box}>
+            {props.onPress && (
+              <TouchableOpacity onPress={props.onPress}>
+                {props.icon}
+              </TouchableOpacity>
+            )}
+            <View style={styles.containerText}>
+              <Text style={styles.title}>
+                {props.title}
+              </Text>
+              <Text style={styles.subtitle}>
+                {props.subtitle}
+              </Text>
+            </View>
           </View>
-        </View>
           {props.onPress && (
             <View style={styles.boxHelp}>
               <TouchableOpacity onPress={props.onPressHelp}>
@@ -54,6 +58,6 @@ export const Header = (props: HeaderProps): ReactElement => {
         {props.children}
         {props.divider && <View style={styles.divider} />}
       </View>
-    </>
+    </View>
   )
 }
