4eefff62c38ece7ae13246bb6894ecb79584149d
Ermesson Lima dos Santos <ermesson@MacBook-Pro-de-Ermesson.local>
2024-06-19 23:00:06 -0300

    fix: title

--- packages/react-native/src/components/CardComments/CardComments.props.ts
+++ packages/react-native/src/components/CardComments/CardComments.props.ts
@@ -1,15 +1,16 @@
 export interface CardCommentsProps {
-  comment: string;
-  skill: string;
-  onPressDelete?: () => void;
-  iconButtonDelete?: React.ReactNode;
-  disabledDelete?: boolean;
-  onPressUpdate?: () => void;
-  iconButtonUpdate?: React.ReactNode;
-  disabledUpdate?: boolean;
-  onPressCheck?: () => void;
-  iconButtonCheck?: React.ReactNode;
-  disabledCheck?: boolean;
-  updateDate?: string;
-  marginBottom?: number;
+  comment: string
+  skill: string
+  onPressDelete?: () => void
+  iconButtonDelete?: React.ReactNode
+  disabledDelete?: boolean
+  onPressUpdate?: () => void
+  iconButtonUpdate?: React.ReactNode
+  disabledUpdate?: boolean
+  onPressCheck?: () => void
+  iconButtonCheck?: React.ReactNode
+  disabledCheck?: boolean
+  updateDate?: string
+  marginBottom?: number
+  title?: string
 }
